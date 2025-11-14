cab9ebf15022c417016253b361a4c441e0cdad9b
Ricardoacliver <ricardoacliver7@gmail.com>
2025-11-15 00:26:18 +0100

    Add multiplication table program in exerc35.cs

---
+++ exerc35.cs
@@ -0,0 +1,20 @@
+using System;
+
+
+class Exerc34
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Valor:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= 13; i++)
+        {
+            Console.WriteLine(n * i);
+
+        }
+
+        Console.ReadKey();
+    }
+
+}
