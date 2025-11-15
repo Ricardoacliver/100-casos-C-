85bacedcb0be5ebae6859af3b36f7bcc07cbc354
Ricardoacliver <ricardoacliver7@gmail.com>
2025-11-15 02:24:27 +0100

    Add employee salary calculation program

---
+++ exerc36.cs
@@ -0,0 +1,39 @@
+using System;
+
+public class Exerc35
+{
+    static void Main(string[] args)
+    {
+
+
+        Console.Write("Digite a quantidade de funcionários:");
+        int qtd = Convert.ToInt32(Console.ReadLine());
+
+        int menor = int.MaxValue;
+
+
+        int mediasal = 0;
+        int i = 0;
+        while (i < qtd)
+        {
+            Console.Write($"Nome o {i + 1} funcionário:");
+            string nome = Console.ReadLine();
+
+            Console.Write($"Salário do {i + 1} funcionário:");
+            int sal = Convert.ToInt32(Console.ReadLine());
+
+
+            i++;
+            mediasal += sal / qtd;
+
+            if (sal < menor)
+            {
+                menor = sal;
+            }
+
+        }
+        Console.WriteLine($"Media dos salários:{mediasal}");
+        Console.WriteLine($"Salários mais baixo:{menor}");
+
+    }
+}
